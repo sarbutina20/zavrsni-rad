9e432e3aab479e9bade9ef54442e5b6794867aba
sarbutina20 <sarbutina20@foi.hr>
2023-08-21 22:12:36 +0200

    Napravljeno bolje upravljanje odgovorima i error.

--- backend/DAO/korisniciDAO.js
+++ backend/DAO/korisniciDAO.js
@@ -1,6 +1,7 @@
 const { ObjectId } = require("mongodb");
 const Baza = require("./baza.js");
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 class KorisniciDAO {
     constructor() {
@@ -11,27 +12,44 @@ class KorisniciDAO {
       try {
           const db = await this.baza.poveziSeNaBazu();
           const baza = db.database;
-
-
           const korisniciKolekcija = baza.collection('korisnici');
-          console.log(korisniciKolekcija);
-
-
+          const ulogeKolekcija = baza.collection('uloge');
   
           const dohvaceniKorisnik = await korisniciKolekcija.findOne({ KorisnickoIme: korisnik.KorisnickoIme });
           
           if (!dohvaceniKorisnik) {
-              console.log("NEma ga")
+              return {error:"Ne postoji korisnik s takvim korisničkim imenom."};
           }
   
           const lozinkaUsporedba = await bcrypt.compare(korisnik.Lozinka, dohvaceniKorisnik.Lozinka);
-  
-          db.prekiniVezu();
-          return lozinkaUsporedba;
+          
+          
+          if(lozinkaUsporedba) {
+
+            const uloga = await ulogeKolekcija.findOne({ _id: dohvaceniKorisnik.Uloga_ID });
+            
+            const payload = {
+                KorisnickoIme: korisnik.KorisnickoIme,
+                Uloga: uloga.Naziv 
+            };
+        
+            const tajniKljuc = process.env.JWT;
+            const token = jwt.sign(payload, tajniKljuc, { expiresIn: '1h' });
+            
+
+            db.prekiniVezu();
+            return { token };
+
+
+          } else {
+            db.prekiniVezu();
+            return {error:"Neispravna lozinka."}
+          }
 
       } catch (error) {
+        db.prekiniVezu();
           console.error('Greška pri prijavi:', error);
-          throw error;
+          return {error:"Greška pri prijavi"}
       }
   }
   
@@ -39,21 +57,34 @@ class KorisniciDAO {
     async registracija(noviKorisnik) {
         const db = await this.baza.poveziSeNaBazu();
         const korisniciKolekcija = db.database.collection('korisnici');
-
         const hashiranaLozinka = await bcrypt.hash(noviKorisnik.Lozinka, 12);
-
-        const noviKorisnikObjekt = {
-            Korisnicko_Ime: noviKorisnik.Korisnicko_Ime,
-            Lozinka: hashiranaLozinka,
-            Email: noviKorisnik.Email,
-            Uloga_ID: ObjectId("64e22057f9497eba62ed9513")
-        };
-
-        const povratneInfo = await korisniciKolekcija.insertOne(noviKorisnikObjekt);
-        db.prekiniVezu();
-
-        return povratneInfo.result.ok;
+        let povratneInfo = null;
+        try {
+            const noviKorisnikObjekt = {
+                KorisnickoIme: noviKorisnik.KorisnickoIme,
+                Lozinka: hashiranaLozinka,
+                Email: noviKorisnik.Email,
+                Uloga_ID: new ObjectId("64e22057f9497eba62ed9513")
+            };
+            povratneInfo = await korisniciKolekcija.insertOne(noviKorisnikObjekt);
+            console.log("Korisnik uspješno unesen")
+        } catch (error) {
+            db.prekiniVezu()
+            if (error.code === 11000) {
+                console.warn('Korisničko ime već postoji.');
+                return {error:"Korisničko ime već postoji."} 
+            } else {
+                console.error('Greška pri unosu korisnika:', error);
+                return {error:"Greška pri unosu korisnika:"}
+            }
+            
+        }
+        db.prekiniVezu()
+        if(povratneInfo) return true;
     }
+
+
+
 }
 
 module.exports = KorisniciDAO;
